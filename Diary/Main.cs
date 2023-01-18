43d3281551c7735eae461b6631c36f8b56619368
cacakfi20 <cacakfi20@sps-prosek.cz>
2023-01-18 13:25:42 +0100

    ez

--- Diary/Main.cs
+++ Diary/Main.cs
@@ -8,82 +8,82 @@ public class MainClass
 {
     
     public static void Save(string name, string descrip, DateTime datum)
-    {
-        JObject json = new JObject();
+    { 
         int x = 0;
         string path = @"events.txt";
         var f = new FileInfo(path);
+        List<string> events = new List<string>();
+        List<Event> events_event = new List<Event>();
+        int last_id = 0;
         if (!File.Exists(path))
         {
             File.Create(path).Dispose();
         }
         if (f.Length != 0)
         {
-            using (StreamReader r = new StreamReader(@"events.txt"))
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
             {
-                string jsonn = r.ReadToEnd();
-                dynamic array = JsonConvert.DeserializeObject(jsonn);
-                foreach(var item in array)
-                {
-                    x++;
-                }
+                events.Add(line);
             }
+            sr.Close();
 
-            if (x != 0)
+            foreach (var a in events)
             {
-                using (StreamReader r = new StreamReader(@"events.txt"))
-                {
-                    string jsonn = r.ReadToEnd();
-                    dynamic array = JsonConvert.DeserializeObject(jsonn);
-                    foreach(var item in array)
-                    {
-                        json.Add(new JProperty(item));
-                    }
-                }
+                Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                events_event.Add(aaa);
+                last_id = aaa.Id + 1;
             }
-            File.WriteAllText(path, String.Empty);
         }
         else
         {
             Console.WriteLine("soubor je prázdný");
         }
-
-        //DateTime date = new DateTime(2020, 7, 15, 7, 0, 0);
-        Event eve = new Event(x, name, descrip, datum);
-        //json.Add(new JProperty(eve.Id.ToString(), eve.Name, eve.Description, eve.Date));
-        string result = JsonConvert.SerializeObject(eve);
-
-        File.WriteAllText(@"events.txt", result);
-
+        Event eve = new Event(last_id, name, descrip, datum);
+        events_event.Add(eve);
+        string eventiky = JsonConvert.SerializeObject(eve);
+        events.Add(eventiky);
+        StreamWriter sw = new StreamWriter(path);
+        foreach (string js in events)
+        {
+            sw.WriteLine(js);
+        }
+        sw.Close();
     }
 
-    public static void Read()
+    public static void Read_By_Date()
     {
         string path = @"events.txt";
         var f = new FileInfo(path);
-        List<Event> events = new List<Event>(); 
+        List<string> events = new List<string>();
+        List<Event> events_event = new List<Event>();
         if (f.Length != 0)
         {
-            using (StreamReader r = new StreamReader(@"events.txt"))
+
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
             {
-                string json = r.ReadLine();
-                dynamic array = JsonConvert.DeserializeObject(json);
-                Console.WriteLine(array);
-                Event eve = JsonSerializer.Deserialize<Event>(json);
+                events.Add(line);
+            }
+            sr.Close();
+
+            foreach (var a in events)
+            {
+                Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                events_event.Add(aaa);
+            }
+
+            events_event = events_event.OrderBy(p => p.Date).ToList();
+            
+            foreach (var eve in events_event)
+            {
                 Console.WriteLine(eve.Id);
+                Console.WriteLine(eve.Name);
+                Console.WriteLine(eve.Description);
                 Console.WriteLine(eve.Date);
-/*
-                foreach(var item in array)
-                {
-                    foreach (var co in item)
-                    {
-                        Console.WriteLine("-----------");
-                        foreach (var c in co)
-                        {
-                            Console.WriteLine(c);
-                        }
-                    }
-                }*/
+                Console.WriteLine("-----------"); 
             }
         }
         else
@@ -91,5 +91,128 @@ public class MainClass
             Console.WriteLine("Nemáte žádné akce");
         }
     }   
+    public static void Read_By_Id()
+    {
+        string path = @"events.txt";
+        var f = new FileInfo(path);
+        List<string> events = new List<string>();
+        List<Event> events_event = new List<Event>();
+        if (f.Length != 0)
+        {
+
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                events.Add(line);
+            }
+            sr.Close();
+
+            foreach (var a in events)
+            {
+                Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                events_event.Add(aaa);
+            }
+
+            events_event = events_event.OrderByDescending(p => p.Id).ToList();
+            
+            foreach (var eve in events_event)
+            {
+                Console.WriteLine(eve.Id);
+                Console.WriteLine(eve.Name);
+                Console.WriteLine(eve.Description);
+                Console.WriteLine(eve.Date);
+                Console.WriteLine("-----------"); 
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nemáte žádné akce");
+        }
+    }   
+    public static void Read_By_Name(string name)
+    {
+        string path = @"events.txt";
+        var f = new FileInfo(path);
+        List<string> events = new List<string>();
+        List<Event> events_event = new List<Event>();
+        if (f.Length != 0)
+        {
+
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                events.Add(line);
+            }
+            sr.Close();
+
+            foreach (var a in events)
+            {
+                Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                events_event.Add(aaa);
+            }
+
+            int x = 0;
+            foreach (var eve in events_event)
+            {
+                if (eve.Name == name)
+                {
+                    Console.WriteLine(eve.Id);
+                    Console.WriteLine(eve.Name);
+                    Console.WriteLine(eve.Description);
+                    Console.WriteLine(eve.Date);
+                    Console.WriteLine("-----------");
+                    x++;
+                }
+            }
+
+            if (x == 0)
+            {
+                Console.WriteLine("Nenašli jsme tuto událost");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nemáte žádné akce");
+        }
+    }
+
+    public static void Delete(int id)
+    {
+        string path = @"events.txt";
+        List<string> events = new List<string>();
+        List<string> updated_events = new List<string>();
+        List<Event> events_event = new List<Event>();
+        StreamReader sr = new StreamReader(path);
+        string line;
+        
+        while ((line = sr.ReadLine()) != null)
+        {
+            events.Add(line);
+        }
+        sr.Close();
+
+        foreach (var a in events)
+        {
+            Event aaa = JsonConvert.DeserializeObject<Event>(a);
+            events_event.Add(aaa);
+        }
+        
+        events_event = events_event.Where(a => a.Id != id).ToList();
+        
+        foreach (var eve in events_event)
+        {
+            string ev = JsonConvert.SerializeObject(eve);
+            updated_events.Add(ev);
+        }
+        StreamWriter sw = new StreamWriter(path);
+
+        foreach (string js in updated_events)
+        {
+            sw.WriteLine(js);
+        }
+        sw.Close();
+    }
 }
 
--- Diary/Program.cs
+++ Diary/Program.cs
@@ -11,29 +11,72 @@ class Diary
         bool menu = true;
         while (menu)
         {
-            Console.WriteLine("1 - přidat, 2 - zobrazit od nejbližších, zobrazit - nejnovější");
+            Console.WriteLine("1 - přidat událost");
+            Console.WriteLine("2 - zobrazit od nejbližších");
+            Console.WriteLine("3 - zobrazit nejnovější");
+            Console.WriteLine("4 - vyhledat podle jména");
+            Console.WriteLine("5 - smazat událost");
             char choice = Console.ReadKey().KeyChar;
             Console.WriteLine();
             switch (choice)
             {
                 case '1':
+                    Console.Clear();
+                    bool nameB = false;
+                    bool dateB = false;
                     var cultureInfo = new CultureInfo("de-DE");
-                    Console.WriteLine("Napiš název události");
-                    string name = Console.ReadLine();
+                    string name = "";
+                    while (!nameB)
+                    {
+                        Console.WriteLine("Napiš název události");
+                        name = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(name) && !string.IsNullOrWhiteSpace(name))
+                        {
+                            nameB = true;
+                        }
+                    }
                     Console.Clear();
                     Console.WriteLine("Napiš popis události (nepovinné)");
                     string descrip = Console.ReadLine();
                     Console.Clear();
-                    Console.WriteLine("Napiš datum (den mesic rok)");
-                    string date = Console.ReadLine();
+                    DateTime datee = DateTime.Now;
+                    while (!dateB)
+                    {
+                        Console.WriteLine("Napiš datum (den mesic rok)");
+                        string date = Console.ReadLine();
+                        try
+                        {
+                            datee = DateTime.Parse(date, cultureInfo, DateTimeStyles.NoCurrentDateDefault);
+                            dateB = true;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Zkontrolujte zda ste napsli datum správně (den měsíc rok)");
+                        }
+                    }
                     Console.Clear();
-                    DateTime datee = DateTime.Parse(date, cultureInfo, DateTimeStyles.NoCurrentDateDefault);
                     MainClass.Save(name, descrip, datee);
                     break;
                 case '2':
-                    MainClass.Read();
+                    Console.Clear();
+                    MainClass.Read_By_Date();
                     break;
                 case '3':
+                    Console.Clear();
+                    MainClass.Read_By_Id();
+                    break;
+                case '4':
+                    Console.Clear();
+                    Console.WriteLine("Jakou událost chceš vyhledat?");
+                    string eve = Console.ReadLine();
+                    MainClass.Read_By_Name(eve);
+                    break;
+                case '5':
+                    Console.Clear();
+                    Console.WriteLine("Napiš id události, kterou chceš smazat");
+                    string id = Console.ReadLine();
+                    int idd = int.Parse(id);
+                    MainClass.Delete(idd);
                     break;
             }
         }
