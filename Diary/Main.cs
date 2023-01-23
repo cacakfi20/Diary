8e3919e020829fd07a1827abd0b615dcbcbfbda4
cacakfi20 <cacakfi20@sps-prosek.cz>
2023-01-23 12:51:45 +0100

    done

--- Diary/Main.cs
+++ Diary/Main.cs
@@ -1,24 +1,25 @@
-﻿using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
-using JsonSerializer = System.Text.Json.JsonSerializer;
+﻿using Newtonsoft.Json;
 
 namespace Diary;
 
 public class MainClass
 {
     
+    // Funkce na ukládání události
     public static void Save(string name, string descrip, DateTime datum)
-    { 
-        int x = 0;
+    {
+        // proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         var f = new FileInfo(path);
         List<string> events = new List<string>();
         List<Event> events_event = new List<Event>();
         int last_id = 0;
+        // kontrola zda soubor na ukládání existuje
         if (!File.Exists(path))
         {
             File.Create(path).Dispose();
         }
+        // kontrola zda není txt soubor prázdný 
         if (f.Length != 0)
         {
             StreamReader sr = new StreamReader(path);
@@ -29,6 +30,7 @@ public class MainClass
             }
             sr.Close();
 
+            // zjišťování posledního id které bude poté přiděleno nové události
             foreach (var a in events)
             {
                 Event aaa = JsonConvert.DeserializeObject<Event>(a);
@@ -40,27 +42,39 @@ public class MainClass
         {
             Console.WriteLine("soubor je prázdný");
         }
+        // instance nové události
         Event eve = new Event(last_id, name, descrip, datum);
         events_event.Add(eve);
         string eventiky = JsonConvert.SerializeObject(eve);
         events.Add(eventiky);
+        
+        // zápis do souboru
         StreamWriter sw = new StreamWriter(path);
         foreach (string js in events)
         {
             sw.WriteLine(js);
         }
         sw.Close();
+        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Událost byla přidána");
+        Console.ResetColor();
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
     }
 
+    // Funkce na čtení od nejbližší události
     public static void Read_By_Date()
     {
+        // Proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         var f = new FileInfo(path);
         List<string> events = new List<string>();
         List<Event> events_event = new List<Event>();
+        // kontrola zda není txt soubor prázdný 
         if (f.Length != 0)
         {
-
+            // čtení ze souboru
             StreamReader sr = new StreamReader(path);
             string line;
             while ((line = sr.ReadLine()) != null)
@@ -69,37 +83,49 @@ public class MainClass
             }
             sr.Close();
 
+            // deserializace na objekty
             foreach (var a in events)
             {
                 Event aaa = JsonConvert.DeserializeObject<Event>(a);
                 events_event.Add(aaa);
             }
-
+            
+            // seřazení podle data události
             events_event = events_event.OrderBy(p => p.Date).ToList();
             
+            // výpis událostí
             foreach (var eve in events_event)
             {
-                Console.WriteLine(eve.Id);
                 Console.WriteLine(eve.Name);
                 Console.WriteLine(eve.Description);
                 Console.WriteLine(eve.Date);
-                Console.WriteLine("-----------"); 
+                Console.WriteLine("-----------------"); 
             }
         }
+        // je možné že bude soubor zcelá prazdný
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Nemáte žádné akce");
+            Console.ResetColor();
         }
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
     }   
+   
+    // Funkce na seřazení události od nejnovějších (podle ID)
     public static void Read_By_Id()
     {
+        // Proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         var f = new FileInfo(path);
         List<string> events = new List<string>();
         List<Event> events_event = new List<Event>();
+        //  Kontrola zda není txt soubor prázdný
         if (f.Length != 0)
         {
-
+            
+            // Čtení ze souboru
             StreamReader sr = new StreamReader(path);
             string line;
             while ((line = sr.ReadLine()) != null)
@@ -108,135 +134,212 @@ public class MainClass
             }
             sr.Close();
 
+            //Deserializace na objekty
             foreach (var a in events)
             {
                 Event aaa = JsonConvert.DeserializeObject<Event>(a);
                 events_event.Add(aaa);
             }
-
-            events_event = events_event.OrderByDescending(p => p.Id).ToList();
             
+            // Seřazení podle ID od největšího po nejmenší
+            events_event = events_event.OrderByDescending(p => p.Id).ToList();
+
+            // Výpis událostí
             foreach (var eve in events_event)
             {
-                Console.WriteLine(eve.Id);
                 Console.WriteLine(eve.Name);
                 Console.WriteLine(eve.Description);
                 Console.WriteLine(eve.Date);
-                Console.WriteLine("-----------"); 
+                Console.WriteLine("-----------------"); 
             }
         }
+        // Možnost že je soubor prázdný
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Nemáte žádné akce");
+            Console.ResetColor();
         }
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
     }   
+    
+    // Funkce na vyhledání události podle jména
     public static void Read_By_Name(string name)
     {
+        // Proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         var f = new FileInfo(path);
         List<string> events = new List<string>();
         List<Event> events_event = new List<Event>();
-        if (f.Length != 0)
+        
+        // Kontrola zda jmén není prázdné
+        if (!string.IsNullOrEmpty(name) && !string.IsNullOrWhiteSpace(name))
         {
-
-            StreamReader sr = new StreamReader(path);
-            string line;
-            while ((line = sr.ReadLine()) != null)
+            // Kontrola zda není soubor prázdný
+            if (f.Length != 0)
             {
-                events.Add(line);
-            }
-            sr.Close();
-
-            foreach (var a in events)
-            {
-                Event aaa = JsonConvert.DeserializeObject<Event>(a);
-                events_event.Add(aaa);
-            }
-
-            int x = 0;
-            foreach (var eve in events_event)
-            {
-                if (eve.Name == name)
+                // Čtení ze souboru
+                StreamReader sr = new StreamReader(path);
+                string line;
+                while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(eve.Id);
-                    Console.WriteLine(eve.Name);
-                    Console.WriteLine(eve.Description);
-                    Console.WriteLine(eve.Date);
-                    Console.WriteLine("-----------");
-                    x++;
+                    events.Add(line);
+                }
+                sr.Close();
+
+                // Deserializace na objekty
+                foreach (var a in events)
+                {
+                    Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                    events_event.Add(aaa);
+                }
+
+                // Vypís nalezené události
+                int x = 0;
+                foreach (var eve in events_event)
+                {
+                    if (eve.Name == name)
+                    {
+                        Console.WriteLine(eve.Name);
+                        Console.WriteLine(eve.Description);
+                        Console.WriteLine(eve.Date);
+                        Console.WriteLine("-----------------");
+                        x++;
+                    }
+                }
+
+                // Pokud nebyla událost nalezena
+                if (x == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Nenašli jsme tuto událost");
+                    Console.ResetColor();
                 }
             }
-
-            if (x == 0)
+            // Možnost že je soubor prázdný
+            else
             {
-                Console.WriteLine("Nenašli jsme tuto událost");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Nemáte žádné akce");
+                Console.ResetColor();
             }
         }
+        // Warning za špatně napsané jméno
         else
         {
-            Console.WriteLine("Nemáte žádné akce");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Jméno nemůže být prázdné");
+            Console.ResetColor();
         }
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
     }
 
+    // Funkce na mazání události
     public static void Delete()
     {
+        
+        // Proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         List<string> events = new List<string>();
         List<string> updated_events = new List<string>();
         List<Event> events_event = new List<Event>();
         StreamReader sr = new StreamReader(path);
         string line;
+        int x = 0;
 
+        // Čtení ze souboru
         while ((line = sr.ReadLine()) != null)
         {
             events.Add(line);
         }
         sr.Close();
 
+        // Deserializace na objekty
         foreach (var a in events)
         {
             Event aaa = JsonConvert.DeserializeObject<Event>(a);
             events_event.Add(aaa);
         }
         
+        // Výpis událostí i s IDčkama
         foreach (var eve in events_event)
         {
             Console.WriteLine(eve.Id);
             Console.WriteLine(eve.Name);
             Console.WriteLine(eve.Description);
             Console.WriteLine(eve.Date);
-            Console.WriteLine("-----------");
+            Console.WriteLine("-----------------");
         }
         
         Console.WriteLine("Napiš id události, kterou chceš smazat");
         string id = Console.ReadLine();
-        int idd = int.Parse(id);
-        
-        events_event = events_event.Where(a => a.Id != idd).ToList();
-        
-        foreach (var eve in events_event)
+        int idd;
+        bool isNumeric;
+        // Kontrola zda je ID validní
+        if (isNumeric = int.TryParse(id, out idd))
         {
-            string ev = JsonConvert.SerializeObject(eve);
-            updated_events.Add(ev);
-        }
-        StreamWriter sw = new StreamWriter(path);
+            Console.Clear();
 
-        foreach (string js in updated_events)
-        {
-            sw.WriteLine(js);
+            // Vytvářim dva identické listy s objektama, jeden pak změním aby tam nebylo vybrané ID --- 
+            List<Event> eventiky = events_event;
+            events_event = events_event.Where(a => a.Id != idd).ToList();
+            
+            // --- pokud jsou stejné idd neexistuje
+            if (eventiky.SequenceEqual(events_event))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Událost s ID {idd} neexistuje");
+                Console.ResetColor();
+            }
+            // info o úspěšném smazání
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Událost s ID {idd} byla úspěsně smazána");
+                Console.ResetColor();
+            }
+        
+            // Zápis zpět do souboru
+            foreach (var eve in events_event)
+            {
+                string ev = JsonConvert.SerializeObject(eve);
+                updated_events.Add(ev);
+            }
+            StreamWriter sw = new StreamWriter(path);
+
+            foreach (string js in updated_events)
+            {
+                sw.WriteLine(js);
+            }
+            sw.Close();
         }
-        sw.Close();
+        // Warning že se nejedná o validní ID
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Nejedná se o validní ID");
+            Console.ResetColor();
+        }
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
     }
     
+    // Funkce na zobrazení zítřejších událostí 
     public static void Read_By_Tomm_Date()
     {
+        
+        // Proměnné potřebné pro kontrolu a práci s listy
         string path = @"events.txt";
         var f = new FileInfo(path);
         List<string> events = new List<string>();
         List<Event> events_event = new List<Event>();
+        // kontrola zda není soubor prázdný 
         if (f.Length != 0)
         {
-
+   
+            // Čtení a deserializace na objekty
             StreamReader sr = new StreamReader(path);
             string line;
             while ((line = sr.ReadLine()) != null)
@@ -251,28 +354,90 @@ public class MainClass
                 events_event.Add(aaa);
             }
 
+            //Zobrazení událostí se zítřejším datem
             int x = 0;
             foreach (var eve in events_event)
             {
                 if (eve.Date == DateTime.Today.AddDays(1))
                 {
-                    Console.WriteLine(eve.Id);
                     Console.WriteLine(eve.Name);
                     Console.WriteLine(eve.Description);
                     Console.WriteLine(eve.Date);
-                    Console.WriteLine("-----------");
+                    Console.WriteLine("-----------------");
                     x++;
                 }
             }
 
+            // Pokud nenajde žádné akce se zítřejším datem
             if (x == 0)
             {
-                Console.WriteLine("Nenašli jsme tuto událost");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Na zítřek nemáte žádne akce");
+                Console.ResetColor();
             }
         }
+        // Případ že je soubor prázdný
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("Nemáte žádné akce");
+            Console.ResetColor();
+        }
+        Console.WriteLine("Pro pokračování stiskni enter-->");
+        Console.ReadLine();
+    }
+    
+    // Funkce na zobrazení dnešních událostí
+    public static void Read_By_Today_Date()
+    {
+        string path = @"events.txt";
+        var f = new FileInfo(path);
+        List<string> events = new List<string>();
+        List<Event> events_event = new List<Event>();
+        if (f.Length != 0)
+        {
+
+            // Čtení a deserializace na objekty
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                events.Add(line);
+            }
+            sr.Close();
+
+            foreach (var a in events)
+            {
+                Event aaa = JsonConvert.DeserializeObject<Event>(a);
+                events_event.Add(aaa);
+            }
+
+            //Zobrazení událostí s dnešním datem
+            int x = 0;
+            foreach (var eve in events_event)
+            {
+                if (eve.Date == DateTime.Today)
+                {
+                    Console.WriteLine(eve.Name);
+                    Console.WriteLine(eve.Description);
+                    Console.WriteLine(eve.Date);
+                    Console.WriteLine("-----------------");
+                    x++;
+                }
+            }
+
+            // Pokud nenajde žádné akce s dnešním datem
+            if (x == 0)
+            {
+                Console.WriteLine("Na dnešek nemáte žádné akce");
+            }
+        }
+        // Případ že je soubor prázdný
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Nemáte žádné akce");
+            Console.ResetColor();
         }
     }
    
--- Diary/Program.cs
+++ Diary/Program.cs
@@ -5,28 +5,36 @@ class Diary
 {
     static void Main()
     {
-        /*
-        string path = @"events.txt";
-        File.WriteAllText(path, String.Empty);*/
         bool menu = true;
+        // While loop na menu
         while (menu)
         {
+            // Zobrazení dnešních akcí
+            Console.Clear();
+            Console.WriteLine("-----------------");
+            Console.WriteLine("DNEŠNÍ AKCE");
+            MainClass.Read_By_Today_Date();
+            Console.WriteLine("-----------------");
+            
             Console.WriteLine("1 - přidat událost");
             Console.WriteLine("2 - zobrazit od nejbližších");
             Console.WriteLine("3 - zobrazit nejnovější");
             Console.WriteLine("4 - zobrazit zítřejší akce");
             Console.WriteLine("5 - vyhledat podle jména");
             Console.WriteLine("6 - smazat událost");
+            Console.WriteLine("7 - ukončit");
             char choice = Console.ReadKey().KeyChar;
             Console.WriteLine();
             switch (choice)
             {
+                // Přidání události
                 case '1':
                     Console.Clear();
                     bool nameB = false;
                     bool dateB = false;
                     var cultureInfo = new CultureInfo("de-DE");
                     string name = "";
+                    // Kontrola názvu eventu (nesmí být prádná)
                     while (!nameB)
                     {
                         Console.WriteLine("Napiš název události");
@@ -37,9 +45,11 @@ class Diary
                         }
                     }
                     Console.Clear();
+                    // event nemusí mít žádný popis
                     Console.WriteLine("Napiš popis události (nepovinné)");
                     string descrip = Console.ReadLine();
                     Console.Clear();
+                    // kontrola datumu (musí se jednat o validní datum)
                     DateTime datee = DateTime.Now;
                     while (!dateB)
                     {
@@ -58,27 +68,36 @@ class Diary
                     Console.Clear();
                     MainClass.Save(name, descrip, datee);
                     break;
+                // Nejbližší události
                 case '2':
                     Console.Clear();
                     MainClass.Read_By_Date();
                     break;
+                // Nejnovější události (podle přidání)
                 case '3':
                     Console.Clear();
                     MainClass.Read_By_Id();
                     break;
+                // zítřejší události
                 case '4':
                     Console.Clear();
                     MainClass.Read_By_Tomm_Date();
                     break;
+                // vyhledávání podle jména události (kontrola až ve funkci)
                 case '5':
                     Console.Clear();
                     Console.WriteLine("Jakou událost chceš vyhledat?");
                     string eve = Console.ReadLine();
                     MainClass.Read_By_Name(eve);
                     break;
+                // mazání podle id
                 case '6':
                     Console.Clear();
                     MainClass.Delete();
+                    break;
+                // ukončí while loop menu -> ukončí aplikaci
+                case '7':
+                    menu = false;
                     break;
             }
         }
